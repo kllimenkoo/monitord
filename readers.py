bc989dcfd74473f78ac32d223cfc2584bab9e3f8
kllimenkoo <kliiimenko.al.123654@gmail.com>
2026-07-28 13:40:59 +0300

    chore: change double quotes to single quotes, rename RamMetrics -> RamRawData

--- readers.py
+++ readers.py
@@ -1,36 +1,36 @@
-from models import RamMetrics, DiskRawData, NetRawData
+from models import RamRawData, DiskRawData, NetRawData
 
 
 def read_cpu_stats() -> tuple[int, ...]:
-    with open("/proc/stat", "r") as f:
+    with open('/proc/stat', 'r') as f:
         cpu_line = f.readline()
 
     fields = cpu_line.strip().split()[1:]
     return tuple(int(i) for i in fields)
 
 
-def read_ram_stats() -> RamMetrics:
-    with open("/proc/meminfo", "r") as f:
+def read_ram_stats() -> RamRawData:
+    with open('/proc/meminfo', 'r') as f:
         ram_stats = {}
         for line in f:
-            key, value = line.strip().split(":")
+            key, value = line.strip().split(':')
             ram_stats[key] = int(value.split()[0])
 
-        return RamMetrics(
-            mem_total=ram_stats["MemTotal"],
-            mem_available=ram_stats["MemAvailable"],
-            swap_total=ram_stats["SwapTotal"],
-            swap_free=ram_stats["SwapFree"],
+        return RamRawData(
+            mem_total=ram_stats['MemTotal'],
+            mem_available=ram_stats['MemAvailable'],
+            swap_total=ram_stats['SwapTotal'],
+            swap_free=ram_stats['SwapFree'],
         )
 
 
 def read_disk_stats() -> dict[str, DiskRawData]:
-    with open("/proc/diskstats", "r") as f:
+    with open('/proc/diskstats', 'r') as f:
         disk_stats = {}
         for line in f:
             fields = line.strip().split()
             device_name = fields[2]
-            if device_name.startswith(("sd", "vd", "nvme", "hd")):
+            if device_name.startswith(('sd', 'vd', 'nvme', 'hd')):
                 key = device_name
                 value = DiskRawData(
                     reads_completed=int(fields[3]),
@@ -45,12 +45,12 @@ def read_disk_stats() -> dict[str, DiskRawData]:
 
 
 def read_net_stats() -> dict[str, NetRawData]:
-    with open("/proc/net/dev", "r") as f:
+    with open('/proc/net/dev', 'r') as f:
         net_stats = {}
         for line in f:
             fields = line.strip().split()
-            intf_name = fields[0].strip(":")
-            if intf_name.startswith(("eth", "ens", "enp", "wlan")):
+            intf_name = fields[0].strip(':')
+            if intf_name.startswith(('eth', 'ens', 'enp', 'wlan')):
                 key= intf_name
                 value = NetRawData(
                     receive_bytes=int(fields[1]),
