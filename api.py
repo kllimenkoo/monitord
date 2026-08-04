aa0658e29af39fe33c1cee6a770684879406b01d
kllimenkoo <kliiimenko.al.123654@gmail.com>
2026-08-04 09:23:47 +0300

    feat: add ram endpoint

--- api.py
+++ api.py
@@ -10,3 +10,13 @@ async def get_cpu() -> list[CpuResponse]:
     rows = await read_cpu_recent(limit=60)
     result = [CpuResponse(timestamp=row['timestamp'], usage_percentage=row['usage_percentage']) for row in rows]
     return result
+
+
+@app.get('/ram')
+async def get_ram() -> list[RamResponse]:
+    rows = await read_ram_recent(limit=60)
+    result = [RamResponse(
+        timestamp=row['timestamp'],
+        mem_usage_percentage=row['mem_usage_percentage'],
+        swap_usage_percentage=row['swap_usage_percentage']) for row in rows]
+    return result
